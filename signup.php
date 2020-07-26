cbd29c7b8dc5d0da32819682c0f59df68fd6ba41
Vardhan Dongre <46850871+vardhandongre@users.noreply.github.com>
2020-07-26 14:41:37 -0500

    header and signup frontend updated
    header and signup frontend updated

--- signup.php
+++ signup.php
@@ -2,10 +2,42 @@
   require "header.php";
 
 ?>
+<!DOCTYPE html>
+<html>
+<style>
+/*body {font-family: Arial, Helvetica, sans-serif;}
+* {box-sizing: border-box}*/
+
+input{
+  padding: 6px;
+  font-size: 14px;
+  border-radius: 4px;
+}
+
+label{
+  width: 240px;
+  display: inline-block;
+}
+.button{
+  display: inline-block;
+  background: #d16500;
+  color: #fff;
+  padding: 8px 30px;
+  margin: 30px 0;
+  border-radius: 20px; 
+  border-radius: 20px;
+  transition: background 0.3s;
+}
+.button:hover{
+  background: #3234ad;
+}
+
+</style>
+<body>
   <main>
     <div class="wrapper-main">
       <section class="section-default">
-        <h1>Signup</h1>
+        <!-- <h1>Signup</h1> -->
         <?php
           if (isset($_GET['error'])) {
             if ($_GET['error'] == "emptyfields") {
@@ -30,17 +62,29 @@
             }
           }
         ?>
-        <form action="includes/signup.inc.php" method="post">
-          <input type="text" name="uid" placeholder="Username">
-          <input type="text" name="mail" placeholder="Email">
-          <input type="password" name="pwd" placeholder="Password">
-          <input type="password" name="pwd" placeholder="Repeat Password">
-          <button type="submit" name="signup-submit">Signup</button>
+        <div id="parent">
+        <form action="includes/signup.inc.php" method="post" id="form_login">
+          <!-- <div id="container"> -->
+            <h1>Signup</h1>
+          <p>
+          <label for="username"><b>User Name</b></label>
+          <input type="text" name="uid" placeholder="Username"></p>
+          <p>
+          <label for="username"><b>Email</b></label>
+          <input type="text" name="mail" placeholder="Email"></p> 
+          <p>
+          <label for="username"><b>Password</b></label>
+          <input type="password" name="pwd" placeholder="Password"></p>
+          <p>
+          <label for="username"><b>Re-Enter Password</b></label>
+          <input type="password" name="pwd" placeholder="Repeat Password"></p>
+          <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
+          <button type="submit" name="signup-submit" class = "button">Signup</button>
+          <!-- </div> -->
         </form>
+      </div>
       </section>
     </div>
   </main>
-
-<?php
-  require "footer.php"
-?>
+</body>
+</html>
